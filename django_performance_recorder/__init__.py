6c74e5c2bc08fd48af9e646dc911cff5e22064cb
Adam Chainz <adam@adamj.eu>
2016-09-19 14:08:21 +0100

    Make assert statement rich in pytest

--- django_performance_recorder/__init__.py
+++ django_performance_recorder/__init__.py
@@ -1,5 +1,21 @@
 # -*- coding:utf-8 -*-
+"""
+isort:skip_file
+"""
 from __future__ import absolute_import, division, print_function, unicode_literals
+
+import six
+
+try:
+    import pytest
+except ImportError:
+    pytest = None
+
+if pytest is not None:
+    if six.PY2:
+        pytest.register_assert_rewrite(b'django_performance_recorder.api')
+    else:
+        pytest.register_assert_rewrite('django_performance_recorder.api')
 
 from .api import record  # noqa: F401
 
