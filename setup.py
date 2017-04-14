0dacbf71ef41cee37b97b1c20face28d4058a56e
Adam Chainz <me@adamj.eu>
2017-04-14 10:31:16 +0100

    Remove unused 'setup.py publish'

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import os
 import re
-import sys
 
 from setuptools import find_packages, setup
 
@@ -18,18 +17,6 @@ def get_version(package):
 
 
 version = get_version('django_perf_rec')
-
-
-if sys.argv[-1] == 'publish':
-    if os.system("pip freeze | grep twine"):
-        print("twine not installed.\nUse `pip install twine`.\nExiting.")
-        sys.exit()
-    os.system("python setup.py sdist bdist_wheel")
-    os.system("twine upload dist/*")
-    print("You probably want to also tag the version now:")
-    print("  git tag -a %s -m 'version %s'" % (version, version))
-    print("  git push --tags")
-    sys.exit()
 
 
 with open('README.rst') as readme_file:
