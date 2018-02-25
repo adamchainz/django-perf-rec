56e0b045ec7ddae4e2a77e788e75cc1966ab6e3b
Adam Johnson <me@adamj.eu>
2018-02-24 20:09:51 -0500

    Add python_requires to setup.py

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         'six',
         'sqlparse>=0.2.0',
     ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     license='MIT',
     zip_safe=False,
     keywords='Django',
