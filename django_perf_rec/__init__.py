b9e114262610111b92254777ff5de2fe51467d35
Adam Johnson <me@adamj.eu>
2019-10-20 12:51:35 +0100

    Convert setup.py metadata to setup.cfg
    Use [setup-py-upgrade](https://pypi.org/project/setup-py-upgrade/) plus some manual editing to move to the declarative metadata format.

--- django_perf_rec/__init__.py
+++ django_perf_rec/__init__.py
@@ -10,5 +10,3 @@ if pytest is not None:
     pytest.register_assert_rewrite("django_perf_rec.api")
 
 from .api import TestCaseMixin, get_record_name, get_perf_path, record  # noqa: F401
-
-__version__ = "4.4.0"
