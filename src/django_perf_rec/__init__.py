f685dbc90fbd04f77274bd27f603b95fe422a87c
Adam Johnson <me@adamj.eu>
2019-12-21 20:17:00 +0000

    black

--- src/django_perf_rec/__init__.py
+++ src/django_perf_rec/__init__.py
@@ -9,7 +9,12 @@ except ImportError:
 if pytest is not None:
     pytest.register_assert_rewrite("django_perf_rec.api")
 
-from django_perf_rec.api import TestCaseMixin, get_record_name, get_perf_path, record  # noqa: E402
+from django_perf_rec.api import (
+    TestCaseMixin,
+    get_record_name,
+    get_perf_path,
+    record,
+)  # noqa: E402
 
 __all__ = [
     "TestCaseMixin",
