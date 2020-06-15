381c57f8b2f2ffef85c545bd5f9f66683aad2283
Adam Johnson <me@adamj.eu>
2020-06-15 12:25:44 +0100

    Drop Django 2.0 and 2.1 support

--- tests/test_orm.py
+++ tests/test_orm.py
@@ -1,4 +1,3 @@
-from django.db.models import Q
 from django.test import SimpleTestCase
 
 from django_perf_rec.orm import patch_ORM_to_be_deterministic
@@ -10,9 +9,3 @@ class PatchORMToBeDeterministicTests(SimpleTestCase):
 
     def test_call_it_again(self):
         patch_ORM_to_be_deterministic()
-
-    def test_q_connector(self):
-        q1 = Q(foo="bar") | Q(bar="foo")
-        _path, args, kwargs = q1.deconstruct()
-        q2 = Q(*args, **kwargs)
-        self.assertEqual(q1, q2)
