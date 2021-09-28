a1c964c9d6d7cb58790d4c22ee55220c3dc15c8c
Adam Johnson <me@adamj.eu>
2021-09-28 15:17:09 +0100

    USE_L10N

--- tests/settings.py
+++ tests/settings.py
@@ -1,6 +1,8 @@
 import os
 from typing import List
 
+import django
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 DEBUG = True
@@ -40,7 +42,9 @@ ROOT_URLCONF = "tests.urls"
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 USE_I18N = True
-USE_L10N = True
+
+if django.VERSION < (4, 0):
+    USE_L10N = True
 
 TEMPLATES = [
     {
