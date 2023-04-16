bb3745e314cfc27ba0368705ba08e61ed01a0986
Gert Van Gool <gertvangool@gmail.com>
2023-04-16 19:23:41 +0000

    Randomize the parametrize list to show order doesn't matter

--- tests/test_pytest_parametrize.py
+++ tests/test_pytest_parametrize.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import random
+
 import pytest
 
 from django_perf_rec import record
@@ -7,8 +9,11 @@ from tests.utils import run_query
 
 pytestmark = [pytest.mark.django_db(databases=("default", "second"))]
 
+VALUES = [42, 73, 1337]
+random.shuffle(VALUES)
 
-@pytest.mark.parametrize("query_param", [42, 73, 1337])
+
+@pytest.mark.parametrize("query_param", VALUES)
 def test_with_parametrize(request, query_param):
     with record():
         run_query("default", f"SELECT {query_param}")
