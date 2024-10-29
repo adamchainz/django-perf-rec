a691eedd0f6f658ea9ffdc43505fad56c5c54657
Adam Johnson <me@adamj.eu>
2024-10-29 09:25:13 +0000

    Drop Django 3.2 to 4.1 support (#644)
    These versions are all EOL since April.

--- tests/settings.py
+++ tests/settings.py
@@ -3,8 +3,6 @@ from __future__ import annotations
 import os
 from typing import Any
 
-import django
-
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 DEBUG = True
@@ -46,9 +44,6 @@ ROOT_URLCONF = "tests.urls"
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 USE_I18N = True
-
-if django.VERSION < (4, 0):
-    USE_L10N = True
 
 TEMPLATES = [
     {
